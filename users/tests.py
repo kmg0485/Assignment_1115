1c8780024ae3db4dea2f11d75a569981b0dca59a
kmg0485 <kmg0485@naver.com>
2022-11-15 09:20:39 +0900

    Init Project

---
+++ users/tests.py
@@ -0,0 +1,16 @@
+from urllib import response
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import UserManager
+
+class UserRegisterationAPIViewTestCase(APITestCase):
+    def test_registeration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username":"test1",
+            "password":"1234",
+        }
+        response = self.client.post(url, user_data)
+        print(response.data)
+        
